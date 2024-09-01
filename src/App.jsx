98105d6a828c97a7796ef393e15f1cb6782eb7cf
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-01 15:39:08 +0200

    changing local directory

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ function App() {
             <p className="read-the-docs">
               Click on the Vite and React logos to learn more
             </p>
+            <h1>Hello</h1>
           </div>
 
       }/>
