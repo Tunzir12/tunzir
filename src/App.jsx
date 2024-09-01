5a713a9aa262499b88a42be9ca00c1b6a3ab0e45
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-01 15:51:25 +0200

    modifying main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <HashRouter>
-      <div>
-      <Routes>
-        <Route path='/' element={    
+  
             <div>
               <a href="https://vitejs.dev" target="_blank">
                 <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -35,12 +32,6 @@ function App() {
             </p>
             <h1>Hello</h1>
           </div>
-
-      }/>
-
-      </Routes>
-      </div>
-      </HashRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//using hashrouter for github
+import { Route, Routes, HashRouter} from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <HashRouter>
+      <Routes>
+        <Route path='/' element={< App />}/>
+
+      </Routes>
+      </HashRouter>
   </StrictMode>,
 )
