db19b4593fd565ca5c8609ce24787f5861487bdd
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-01 02:44:41 +0200

    github pages init

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//using hashrouter for github
+import { Route, Routes, HashRouter} from 'react-router-dom'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
+    <HashRouter>
+      <Routes>
+        <Route path='/'/>
+      </Routes>
+    </HashRouter>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // GitHub repository name
+  base: '/Tunzir12/tunzir/', // GitHub repository name
 })
