93adf13cca2b9343bea9e09a00a4183bcac037bb
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-02 21:53:48 +0200

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-//using hashrouter for github
-import { Route, Routes, HashRouter} from 'react-router-dom'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-  
-            <div>
-              <a href="https://vitejs.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-            <h1>Vite + React</h1>
-            <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-              </button>
-              <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
-            </div>
-            <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-            </p>
-            <h1>Hello</h1>
-          </div>
+    <>
+    <div className="container h-screen">
+      <nav className='bg-purple-300 '>
+        <ul className='inline-flex space-x-9 text-xl'>
+          <li className='float-left'><a href="/">Maliha</a></li>
+          <li className='float-right'><a href="">Projects</a></li>
+          <li><a href="">Blog</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+      <div className="section">
+        
+          <h1>Hello! this is Maliha Tunzira.</h1>
+          <p className='text-3xl'>I dont know what to write. but I am not going to be anxious. I will write whatever i want and move on. because i am not publishing it yet</p>
+      </div>
+    </div>
+    </>
   )
 }
 
