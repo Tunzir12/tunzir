6327a5367f7966725f0b01ebed6bb27eb6f617ef
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-04 23:40:58 +0200

    more on ux designing

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ function App() {
     <>
     <div className="container h-screen">
       <nav className='bg-purple-300 '>
-        <div className="flex flex-row">
+        <div className="flex flex-row font-bold text-white">
           <Link to="/" className='basis-1/4'>Home</Link>
           <Link to="/about" className='basis-1/3'>About</Link>
           <Link to="/blog" className='basis-1/3'>Blog</Link>
           <Link to="/contact" className='basis-1/3'>Contact</Link>
         </div>
       </nav>
-      <div className="section">
+      <div className="section p-4">
         
-          <h1>Hello! this is Maliha Tunzira.</h1>
+          <h1 className='decoration-from-font text-2xl'>Hello! this is Maliha Tunzira.</h1>
           <p className='text-3xl'>I dont know what to write. but I am not going to be anxious. I will write whatever i want and move on. because i am not publishing it yet</p>
       </div>
     </div>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import '../App.css'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="container h-screen">
+      <nav className='bg-purple-300 '>
+        <div className="flex flex-row font-bold text-white">
+          <Link to="/" className='basis-1/4'>Home</Link>
+          <Link to="/about" className='basis-1/3'>About</Link>
+          <Link to="/blog" className='basis-1/3'>Blog</Link>
+          <Link to="/contact" className='basis-1/3'>Contact</Link>
+        </div>
+      </nav>
+
+    </div>
   )
 }
 
