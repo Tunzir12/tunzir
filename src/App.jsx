225254ed8cb2d309e3bb61d954cee4ed3d2aa6e3
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-09 10:01:19 +0200

    adding some frontend beauty

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,24 @@ function App() {
   return (
     <>
     <div className="container h-screen">
-      <nav className=' bg-white font-nanum'>
-        <div className="flex flex-row font-bold  text-3xl">
+      <nav className=' bg-white font-nanum p-10'>
+        <div className="flex flex-row font-bold text-indigo-600 text-3xl">
           <Link to="/" className='basis-1/4'>Home</Link>
-          <Link to="/about" className='basis-1/3'>About</Link>
+          <Link to="/about" className='basis-1/3'>About Me</Link>
           <Link to="/blog" className='basis-1/3'>Blog</Link>
           <Link to="/contact" className='basis-1/3'>Contact</Link>
         </div>
       </nav>
       <section>
-        <div className="p-4">
-          <h1 className='font-nerko p-6 text-4xl'>Hello! this is Maliha Tunzira.</h1>
+        <div className="p-4 font-nanum text-3xl ">
+          <h1 className='font-nerko p-6 text-4xl text-purple-600'>Hello! this is Maliha Tunzira.</h1>
           <p className='text-xl'> Welcome to my page!
             <br />
             This project is for self-learning projects. Where I learn different functions and its use everyday. If some page is not working then it is under construction.
             </p>
       </div>
       </section>
-      <footer>
+      <footer className='font-nanum'>
         <div className="p-20 flex flex-row space-x-6 justify-center text-xl">
           <caption>Follow me on: </caption>
           <a href="https://www.linkedin.com/in/m16tunzi/" target='_blank'>
