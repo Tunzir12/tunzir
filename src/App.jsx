1f1e634ec7a0bbd11b55b2482bb8b769b0c35ac8
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-12 11:57:33 +0200

    changing colors

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,20 @@ function App() {
 
   return (
     <>
-    <div className="container h-screen">
-      <nav className=' bg-white font-bebas p-10'>
-        <div className="flex flex-row font-bold text-2xl">
-          <Link to="/" className='basis-1/4 font-logo text-3xl font-extrabold'>MT</Link>
-          <Link to="/about" className='basis-1/3'>About Me</Link>
-          <Link to="/blog" className='basis-1/3'>Blog</Link>
-          <Link to="/contact" className='basis-1/3'>Contact</Link>
+    <div className="container h-screen max-w-full">
+      <nav className='font-bebas'>
+        <div className="flex justify-between items-center px-4 py-2 text-2xl">
+          <Link to="/" className='font-logo text-3xl font-extrabold'>MT</Link>
+          <div className="space-x-10">
+            <Link to="/about" className='hover:outline outline-offset-4'>About Me</Link>
+            <Link to="/blog" className='hover:outline outline-offset-4'>Blog</Link>
+            <Link to="/contact" className='hover:outline outline-offset-4'>Contact</Link>
+          </div>
         </div>
       </nav>
-      <section className='justify-center'>
+      <section className='justify-center p-10'>
         <div className="p-4 font-nanum text-3xl ">
-          <h1 className='font-nerko p-6 text-4xl text-purple-600'>Hello! this is Maliha Tunzira.</h1>
+          <h1 className='font-nerko p-6 text-4xl '>Hello! this is Maliha Tunzira.</h1>
           <p className='text-3xl'> Welcome to my page!
             <br />
             This project is for self-learning projects. Where I learn different functions and its use everyday. If some pages are not working then it is under construction.
@@ -29,10 +31,10 @@ function App() {
       <footer className='font-nanum'>
         <div className="p-20 flex flex-row space-x-6 justify-center text-xl">
           <span>Follow me on: </span>
-          <a href="https://www.linkedin.com/in/m16tunzi/" target='_blank' className='text-purple-600'>
+          <a href="https://www.linkedin.com/in/m16tunzi/" target='_blank' className=''>
             < FontAwesomeIcon icon={faLinkedin} /> 
           </a>
-          <a href="https://github.com/Tunzir12" target='_blank' className='text-purple-600'>
+          <a href="https://github.com/Tunzir12" target='_blank' className=''>
              <FontAwesomeIcon icon={faGithub} />
           </a>
           <button className='bg-blue-500'>
