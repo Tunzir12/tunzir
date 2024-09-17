e7ce4c94bcff1e66b5c36595a1e4e9ebd63762c4
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-17 22:06:39 +0200

    adding dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,54 @@ import { Link } from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faGithub,  faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import profile from './assets/profile.jpeg'
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faEnvelope, faHandPointRight, faMoon, faSun } from '@fortawesome/free-regular-svg-icons'
+import { useState } from 'react'
+
 
 function App() {
 
-  return (
-    <>
-    <div className="h-screen">
+  const [darkMode, setDarkMode] = useState(false)
 
-      <nav className='font-open'>
+  const toggleDark = () => {
+    setDarkMode(!darkMode);
+    // Toggle the data attribute on the root element for dark mode
+    if (darkMode) {
+      document.documentElement.removeAttribute('data-theme')
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark')
+    }
+  }
+
+  return (
+
+    <div className="h-screen w-full dark:bg-blue-950">
+
+      <nav className='font-open dark:text-purple-500'>
           <div className="space-x-48 font-bold text-2xl">
             <Link to="/" className='hover:outline outline-offset-8 rounded-lg'>Home</Link>
             <Link to="/about" className='hover:outline outline-offset-8 rounded-lg'>About Me</Link>
             <Link to="/project" className='hover:outline outline-offset-8 rounded-lg' >Projects</Link>
             <Link to="/contact" className='hover:outline outline-offset-8 rounded-lg'>Contact</Link>
+            <button onClick={toggleDark}>
+              
+              {darkMode ? <FontAwesomeIcon icon={faSun}/> : <FontAwesomeIcon icon={faMoon}/>}
+            </button>
           </div>
       </nav>
 
-      <section className='pt-32 pr-28 pl-28 grid grid-cols-4'>
+      <section className='pt-32 pr-28 space-x-5 pl-28 grid grid-cols-4'>
         <div className=" pt-48 pl-16 text-left font-sans text-3xl col-span-3 ">
-          <h1 className='font-open font-bold text-4xl '>Hi! I am <span className='name'>Maliha Tunzira</span>.</h1>
+          <h1 className='font-open font-bold text-5xl '>Hi! I am <span className='name'>Maliha Tunzira</span>.</h1>
           <p className='text-xl'>
-             As a software engineer, I am passionate about creating software solutions that are user-friendly, accessible and environmental friendly.
+             Software engineer | I am passionate about creating software solutions that are user-friendly, accessible and environmental friendly.
             </p>
+          <button className='rounded-lg' >
+            <Link to="/about" className='' >
+            Learn more about me.
+            </Link>
+            
+
+          </button>
         </div>
 
         <div className='col-span-1'>
@@ -35,8 +60,8 @@ function App() {
       </section>
 
 
-      <footer className='font-nanum bg-gray-400 fixed bottom-0 w-full'>
-        <div className=" flex flex-row space-x-6 justify-center text-xl">
+      <footer className='font-nanum bg-gray-100 fixed bottom-0 w-full'>
+        <div className=" flex flex-start space-x-6 justify-center text-xl">
           <a href="https://www.linkedin.com/in/m16tunzi/" target='_blank' className=''>
             < FontAwesomeIcon icon={faLinkedin} /> 
           </a>
@@ -50,7 +75,6 @@ function App() {
       </footer>
 
     </div>
-    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 
 
 export default {
+  darkMode:[
+    'class','[data-theme="dark"]',
+  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
