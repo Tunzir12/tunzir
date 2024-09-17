ed47c9a1c63b9dd0d2ec9241b342d67b8897b807
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-17 23:46:22 +0200

    updating About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,6 +9,27 @@ const About = () => {
     <div className="container h-screen dark:bg-slate-700">
     <Navbar />
 
+    <section className='grid grid-cols-3'>
+      <div className="objective col-span-2 dark:text-white">
+        <p>I am Maliha, a passionate software engineer, based on Linkoping, Sweden, originally from Dhaka, Bangladesh. I moved to Sweden to pursue Master's degree in Computer Science in 2023, since then I fell in love with the nature and its people. </p>
+      </div>
+      <div className="linkoping">
+        <img src="" alt="" />
+      </div>
+    </section>
+
+    <section>
+      <div className="skills">
+
+      </div>
+    </section>
+
+    <section>
+      <div className="hobbies">
+
+      </div>
+    </section>
+
     </div>
   )
 }
