854c024b1ae1a3fe7ca54a616c27ee8e04f1b7a5
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-20 23:15:50 +0200

    coloring about me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,13 +6,13 @@ import Navbar from '../components/navbar'
 
 const About = () => {
   return (
-    <div className="container h-screen dark:bg-slate-700">
+    <div className="container h-screen dark:bg-blue-950">
     <Navbar />
 
-    <section className='pt-8'>
+    <section className='pt-8 p-20'>
       <div className="objective p-16 dark:text-white">
         <h1 className='text-3xl text-left pl-8 font-bold'>About me</h1>
-        <div className="container">
+        <div className="aboutme dark:bg-purple-950">
         <p className='text-xl'>Hi! I am Maliha, currently pursuing a Master's in Computer Science at Linköping University, building on a solid foundation from my Bachelor's in Computer Science and Engineering from East West University. Being experienced in software quality assurance, with a background in manual testing, API testing, and performance testing, I am proficient in programming languages including Python, C++, JavaScript, and React.js. </p>
         </div>
       </div>
