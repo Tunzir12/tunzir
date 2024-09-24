cf1565d5ccd38bd3502a276ff672e28b1f4ed695
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-24 12:30:09 +0200

    changing the design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import { Link } from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faGithub,  faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import profile from './assets/profile.jpeg'
 import { faEnvelope, faMoon, faSun } from '@fortawesome/free-regular-svg-icons'
 import Navbar from './components/navbar'
 
@@ -12,42 +11,39 @@ function App() {
 
   return (
 
-    <div className="h-screen w-full dark:bg-blue-950">
+    <div className="h-screen">
 
-      < Navbar />
+      <div className="navbar p-14 max-w-5xl text-left">
+        <FontAwesomeIcon className='text-2xl' icon={faMoon} />
+      </div>
 
-      <section className='pt-32 pr-28 space-x-5 pl-28 grid grid-cols-4'>
-        <div className=" pt-48 pl-16 text-left font-sans text-3xl col-span-3 dark:text-white">
-          <h1 className='font-open font-bold text-5xl '>Hi! I am <span className='name'>Maliha Tunzira</span>.</h1>
-          <p className='text-xl'>
-             Software engineer | I am passionate about creating software solutions that are user-friendly, accessible and environmental friendly.
-            </p>
-          <button className='rounded-lg' >
-            <Link to="/about" className='' >
-            Learn more about me.
-            </Link>
-          </button>
+
+      <div className="grid grid-cols-6">
+
+        {/*social sidebar*/}
+        <div className="text-left col-span-1 pt-20">
+            <ul className='text-2xl p-1 space-y-3'>
+            <li><FontAwesomeIcon icon={faLinkedin} /></li>
+            <li><FontAwesomeIcon icon={faGithub} /></li>
+            </ul>
         </div>
 
-        <div className='col-span-1'>
-          <img src={profile} alt='profile pic' className='object-fit rounded-xl'/>
-        </div>
-      </section>
+          {/*Jumbtron */}
+          <div className="jumbo col-span-3 text-left pt-20">
+            <h1 className='font-bold text-5xl'>Hi ! I am Maliha Tunzira.</h1>
+            <p className='text-xl pt-10 pr-10'>Software engineer | I am passionate about creating software solutions that are user-friendly, accessible and environmental friendly.</p>
+          </div>
 
-
-      <footer className='font-nanum fixed bottom-0 w-full'>
-        <div className=" flex flex-start space-x-6 justify-center text-xl">
-          <a href="https://www.linkedin.com/in/m16tunzi/" target='_blank' className=''>
-            < FontAwesomeIcon icon={faLinkedin} /> 
-          </a>
-          <a href="https://www.linkedin.com/in/m16tunzi/" target='_blank' className=''>
-            < FontAwesomeIcon icon={faEnvelope} /> 
-          </a>
-          <a href="https://github.com/Tunzir12" target='_blank' className=''>
-             <FontAwesomeIcon icon={faGithub} />
-          </a>
+        <div className="col-span-2 pt-24 pl-20 pr-10">
+          <ul className='space-y-14 text-4xl font-bold'>
+          <li className='text-right'>
+            <Link className='bg-red-500 pl-4 pr-4 rounded-2xl'>About</Link>
+          </li>
+          <li className='text-right pr-24'><Link className='bg-red-500 pl-4 pr-4 rounded-2xl'>Projects</Link></li>
+          <li className='text-right'><Link className='bg-red-500 pl-4 pr-4 rounded-2xl'>Contact</Link></li>
+          </ul>
         </div>
-      </footer>
+      </div>
 
     </div>
   )
