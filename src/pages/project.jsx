f90bfc1d04bf5af73959df46ec0c281788d9b6b8
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-28 12:27:12 +0200

    adding project page into the system

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,20 +1,44 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import '../App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faCircleDot, faMoon } from '@fortawesome/free-regular-svg-icons'
+import { Link } from 'react-router-dom'
+
 
 const Project = () => {
+
+
   return (
-    <div>
-      <nav className=' bg-white font-nanum p-10'>
-        <div className="flex flex-row font-bold text-indigo-600 text-3xl">
-          <Link to="/" className='basis-1/4'>
-            <FontAwesomeIcon icon={faArrowLeft} />
-            Go to Homepage
-          </Link>
+
+    <div className="min-h-screen bg-gradient-to-r from-Beige to-white to-90%">
+      {/*switch to dark mode */}
+      <div className="navbar p-14 text-left">
+        <FontAwesomeIcon className='text-2xl' icon={faMoon} />
+      </div>
+
+      {/*Navbar */}
+      <div className="navlink grid grid-cols-2 font-bold">
+        <div className="col-p-1 text-left">
+          <Link to={'/'} className='pl-32'>Home</Link>
         </div>
-      </nav>
-  </div>
+
+        <div className="col-p-1 text-right ">
+          <Link to={'/project'} className='pr-32'>Contact</Link>
+        </div>
+      </div>
+
+      {/*Body */}
+      <div className="p-32 text-left">
+      <div className="details p-10 bg-trans-box">
+          <h1 className='font-bold'>Trainee QA Engineer</h1>
+          <h1 className='font-bold'>Vcube SOft & Tech</h1>
+          <h1 className='font-bold'>October,2021 - October,2022</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque harum doloribus asperiores soluta rem impedit delectus culpa commodi suscipit, tempora eaque voluptatem odio ipsa veniam ea corrupti iusto odit!</p>
+        </div>
+      </div>
+
+    </div>
+    
   )
 }
 
