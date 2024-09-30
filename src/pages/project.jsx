3f25c8a5d29caa99d9e088d9990f6edbaa982941
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-30 21:13:45 +0200

    project page done

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleDot, faMoon } from '@fortawesome/free-regular-svg-icons'
+import { faMoon } from '@fortawesome/free-regular-svg-icons'
 import { Link } from 'react-router-dom'
 
 
@@ -30,10 +30,11 @@ const Project = () => {
       {/*Body */}
       <div className="p-32 text-left">
         <div className="grid grid-cols-2 gap-3">
+          {/*project 1 */}
           <div className="details p-10 bg-trans-box space-y-4 col-span-1">
-              <a href='/' className='font-bold text-xl'>Online Portfolio</a>
+              <h1 className='font-bold text-xl'>Online Portfolio</h1>
               <div className="flex flex-row space-x-6">
-                <h1 className='font-bold'>Skills</h1>
+                <h1 className='font-bold'>Tools</h1>
                 <span>JavaScript</span>
                 <span>Vite</span>
                 <span>React.JS</span>
@@ -43,21 +44,26 @@ const Project = () => {
               <button className='p-4 bg-ghiya'><a href="https://github.com/Tunzir12/tunzir" target='_blank'>View the project on github</a></button>
             </div>
 
+          {/*project 1 */}
             <div className="details p-10 bg-trans-box space-y-4 col-span-1">
-              <a href='/' className='font-bold text-xl'>Online Portfolio</a>
+              <h1 className='font-bold text-xl'>Text-editor</h1>
               <div className="flex flex-row space-x-6">
-                <h1 className='font-bold'>Skills</h1>
-                <span>JavaScript</span>
-                <span>Vite</span>
-                <span>React.JS</span>
-                <span>Figma</span>
+                <h1 className='font-bold'>Tools</h1>
+                <span>C++</span>
               </div>
               <p>The text-editor is a module developed using C++ in the Visual Studio Code IDE. It leverages C++'s standard library, containers, and algorithms to provide a range of text manipulation functionalities. The key features of this text editor include: Text search functionality, Text removal capabilities, Text replacement options, Text printing from files. One of the significant aspects of this project is its modularity and extensibility. It allows other developers to easily add to the codebase, implement new features, and improve existing ones.</p>
               <button className='p-4 bg-ghiya'><a href="https://github.com/Tunzir12/Text-editor" target='_blank'>View the project on github</a></button>
             </div>
+
           </div>
+          
+
       </div>
 
+      <div className="footer text-center p-4">
+        <span className='text-ghiya'>Visit My <a href="https://github.com/Tunzir12" target='_blank'>Github</a> page for more projects</span>
+      </div>
+      
     </div>
     
   )
