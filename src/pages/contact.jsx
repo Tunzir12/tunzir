2b4c566199e591e2a072fb688f82b4de70da4384
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-30 22:26:57 +0200

    contact pending work

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -28,21 +28,36 @@ const Contact = () => {
       </div>
 
       {/*Body */}
-      <div className="p-32">
-        <div className="form">
-          <form >
-          <label htmlFor="">Name:</label>
-          <input type="text" name="" id="" placeholder='Name' required/>
-          <label htmlFor="">Email:</label>
-          <input type="email" name="" id="" placeholder='Email' required/>
-          <label htmlFor="">Subject:</label>
-          <input type="text" name="" id="" placeholder='Subject'/>
-          <label htmlFor="">Message:</label>
-          <textarea name="message" id="" placeholder='Message'></textarea>
-          </form>
-        </div>
-          
+      <div className="pl-32 pt-20 pr-32 ">        
+        <div className="grid grid-cols-3">
+          <div className="pl-40 pr-40 col-span-2 flex flex-col space-y-3 text-left">
+            <div>
+              <label className='font-bold'>Name</label><br />
+              <input type="text" className='rounded-lg w-full bg-trans-box' />
+            </div>
+            <div>
+              <label className='font-bold'>Email</label>
+              <input type="email" className='rounded-lg w-full' />
+            </div>
+            <div>
+              <label className='font-bold'>Subject</label>
+              <input type="text" className='rounded-lg w-full' />
+            </div>
+            <div>
+              <label className='font-bold'>Mesasge</label>
+              <textarea name="" className='rounded-lg w-full' id=""></textarea>
+            </div>
+            <div className="text-right">
+            <button className='font-bold p-2 bg-ghiya w-1/3'>Submit</button>
 
+            </div>
+          </div>
+          <div className="pr-40 col-span-1">
+            <div className="p-28 bg-ghiya">
+
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="footer text-center p-4">
