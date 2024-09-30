fef4bcd9a4c116f3f8a349b3b4a826d202caab5c
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-09-30 21:05:00 +0200

    project page

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -29,17 +29,33 @@ const Project = () => {
 
       {/*Body */}
       <div className="p-32 text-left">
-      <div className="details p-10 bg-trans-box">
-          <h1 className='font-bold'>Online Portfolio</h1>
-          <div className="flex flex-row space-x-6">
-            <h1 className='font-bold'>Skills</h1>
-            <span>JavaScript</span>
-            <span>Vite</span>
-            <span>React.JS</span>
+        <div className="grid grid-cols-2 gap-3">
+          <div className="details p-10 bg-trans-box space-y-4 col-span-1">
+              <a href='/' className='font-bold text-xl'>Online Portfolio</a>
+              <div className="flex flex-row space-x-6">
+                <h1 className='font-bold'>Skills</h1>
+                <span>JavaScript</span>
+                <span>Vite</span>
+                <span>React.JS</span>
+                <span>Figma</span>
+              </div>
+              <p>This is a responsive online portfolio built using Vite and React.js. The tech stack includes HTML, CSS, Tailwind CSS, JavaScript, and React Router Dom for navigation. By developing this project from scratch, I've enhanced my skills in modern web design, front-end development, and deployment processes. Through this project, I've gained valuable experience in creating responsive layouts, implementing smooth user interactions, and optimizing performance using Vite's build tools. The source code is managed using Git, allowing for version control and easy updates. </p>
+              <button className='p-4 bg-ghiya'><a href="https://github.com/Tunzir12/tunzir" target='_blank'>View the project on github</a></button>
+            </div>
+
+            <div className="details p-10 bg-trans-box space-y-4 col-span-1">
+              <a href='/' className='font-bold text-xl'>Online Portfolio</a>
+              <div className="flex flex-row space-x-6">
+                <h1 className='font-bold'>Skills</h1>
+                <span>JavaScript</span>
+                <span>Vite</span>
+                <span>React.JS</span>
+                <span>Figma</span>
+              </div>
+              <p>The text-editor is a module developed using C++ in the Visual Studio Code IDE. It leverages C++'s standard library, containers, and algorithms to provide a range of text manipulation functionalities. The key features of this text editor include: Text search functionality, Text removal capabilities, Text replacement options, Text printing from files. One of the significant aspects of this project is its modularity and extensibility. It allows other developers to easily add to the codebase, implement new features, and improve existing ones.</p>
+              <button className='p-4 bg-ghiya'><a href="https://github.com/Tunzir12/Text-editor" target='_blank'>View the project on github</a></button>
+            </div>
           </div>
-          <h1 className='font-bold'>October,2021 - October,2022</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque harum doloribus asperiores soluta rem impedit delectus culpa commodi suscipit, tempora eaque voluptatem odio ipsa veniam ea corrupti iusto odit!</p>
-        </div>
       </div>
 
     </div>
