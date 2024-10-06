754ef3d7b0031ff6cfd2c1289140fdb106763df4
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-10-06 04:06:49 +0200

    responsive failed homepage

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className="min-h-screen bg-gradient-to-r from-amber-100 to-neutral-50 to-90% dark:bg-gradient-to-r dark:from-fuchsia-900 dark:to-blue-950 dark:text-white">
       < ModeSwitch />
 
-      <div className="grid grid-cols-6 lg:pt-16">
+      <div className="grid grid-cols-6 md:max-lg:pt-16">
 
         {/*social sidebar*/}
         <div className="text-left col-span-1 pt-24 ">
@@ -25,12 +25,12 @@ function App() {
         </div>
 
         {/*Jumbtron */}
-        <div className="jumbo lg:col-span-3 lg:text-left lg:pt-20 md:col-span-5">
+        <div className="jumbo col-span-3 text-left pt-20 sm:max-md:col-span-5">
           <h1 className='font-bold text-5xl'>Hi ! I am Maliha Tunzira.</h1>
           <p className='text-xl pt-10 pr-10'>Software engineer | I am passionate about creating software solutions that are user-friendly, accessible and environmental friendly.</p>
         </div>
 
-        <div className=" navlinks col-span-2 pt-24 pl-20 lg:pl-10 pr-10 max-md:hidden">
+        <div className=" navlinks col-span-2 pt-24 pl-20 pr-10 md:max-lg:pl-10 md:max-lg:pt-20 sm:max-md:hidden">
           <ul className='space-y-14 text-4xl font-bold'>
             <li className='text-right  hover:text-6xl '>
               <Link to={'/about'} className='ring-offset-4 bg-orange-200 pl-8 pr-8 rounded-2xl dark:bg-fuchsia-800'>About</Link>
@@ -45,17 +45,18 @@ function App() {
         </div>
 
       </div>
-      <div className="p-16 justify-center flex flex-row space-x-10 font-bold md:hidden">
 
-            <div className='hover:text-6xl '>
-              <Link to={'/about'} className='ring-offset-4 bg-orange-200 pl-8 pr-8 rounded-2xl dark:bg-fuchsia-800'>About</Link>
-            </div>
-            <div className='hover:text-6xl'>
-              <Link to={'/project'} className='ring-offset-4 bg-orange-200 pl-8 pr-8 rounded-2xl dark:bg-fuchsia-800'>Projects</Link>
-            </div>
-            <div className='hover:text-6xl'>
-              <Link to={'/contact'} className='ring-offset-4 bg-orange-200 pl-8 pr-8 rounded-2xl dark:bg-fuchsia-800'>Contact</Link>
-            </div> 
+      <div className="p-16 justify-center flex flex-row space-x-10 font-bold md:max-2xl:hidden">
+
+          <div className='hover:text-6xl '>
+            <Link to={'/about'} className='ring-offset-4 bg-orange-200 pl-8 pr-8 rounded-2xl dark:bg-fuchsia-800'>About</Link>
+          </div>
+          <div className='hover:text-6xl'>
+            <Link to={'/project'} className='ring-offset-4 bg-orange-200 pl-8 pr-8 rounded-2xl dark:bg-fuchsia-800'>Projects</Link>
+          </div>
+          <div className='hover:text-6xl'>
+            <Link to={'/contact'} className='ring-offset-4 bg-orange-200 pl-8 pr-8 rounded-2xl dark:bg-fuchsia-800'>Contact</Link>
+          </div> 
 
       </div>
 
