d1e3acb167410c85a778c047f74164fc8009bc86
Maliha Tunzira <mtunzira1@gmail.com>
2026-04-06 15:24:36 +0200

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
-  base: '/tunzir/', // GitHub repository name
+  base: '/tunzira/', // GitHub repository name
   server: {
     hmr: {
       protocol: 'ws',
